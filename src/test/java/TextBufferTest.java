0fe4da1004c60363218ffc60fd1b89a434167a6c
Gupta Varun Vedic <guptagivarun@gmail.com>
2023-11-20 22:33:43 +0100

    step3_couverture_tests : coverage 80% TestTextBuffer

--- src/test/java/TextBufferTest.java
+++ src/test/java/TextBufferTest.java
@@ -1,7 +1,64 @@
 import fr.einfolearning.tp2.metiers.TextBuffer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 public class TextBufferTest {
+
+    /**
+     * 
+     */
+    @Test
+    public void TextBufferTestIns(){
+        TextBuffer textBuffer = new TextBuffer("Test");
+        
+        // Valide 
+        textBuffer.ins("_ok",4);
+        assertThat(textBuffer.toString(),is("Test_ok"));
+
+        // Invalide 
+        textBuffer.ins("bonjour",16);
+        assertThat(textBuffer.toString(),is("Test_ok"));
+        
+
+    }
+
+    @Test
+    public void TextBufferTestDel(){
+        TextBuffer textBuffer = new TextBuffer("Test_ok");
+        
+        // Valide 
+        assertThat(textBuffer.toString(),is("Test"));
+
+        // Invalide 
+        textBuffer.del(-18,16);
+        assertThat(textBuffer.toString(),is("Test_ok"));
+        
+
+    }
+
+    @Test
+    public void TextBufferTestSubstr(){
+        TextBuffer textBuffer = new TextBuffer("Test_ok");
+        
+        // Valide 
+        assertThat(textBuffer.substr(0,4),is("Test"));
+
+        // Invalide 
+        assertThat(textBuffer.substr(-2,15),is("Test_ok"));
+        
+    }
+
+    @Test
+    public void TextBufferTestMaxP(){
+        TextBuffer textBuffer = new TextBuffer("Test_ok");
+        
+        // Valide 
+        assertThat(textBuffer.maxP(),is(7));
+
+    }
+
     @Test
     public void TextBufferTestToString(){
         TextBuffer bu = new TextBuffer("test_ok");
@@ -12,15 +69,6 @@ public class TextBufferTest {
         Assertions.assertEquals(expectedText,bu_string);
     }
 
-    @Test
-    public void TextBufferTestMaxP(){
-        TextBuffer bu = new TextBuffer("jem'appellehenryjevoudraisbienreussirmavie");
-        int bu_maxP = bu.maxP();
-
-        int expectInt = bu.toString().length();
-
-        Assertions.assertEquals(expectInt,bu_maxP);
-
-    }
+    
 
 }
