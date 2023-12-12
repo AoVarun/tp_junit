07179d9a77fdf21246cd9f3a5814e3283362eea2
bl <bouchkhaf@gmail.com>
2023-12-12 11:02:38 +0100

    step5_tests_mockito: testYankPop

--- src/test/java/TextEditorTest.java
+++ src/test/java/TextEditorTest.java
@@ -1,2 +1,35 @@
+import fr.einfolearning.tp2.metiers.EmacsKillRing;
+import fr.einfolearning.tp2.metiers.TextBuffer;
+import fr.einfolearning.tp2.metiers.TextEditor;
+import fr.einfolearning.tp2.metiers.exceptions.EmacsKillRingOverflowException;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 public class TextEditorTest {
+
+    @Mock
+    public TextBuffer textBuffer;
+    @Mock
+    public EmacsKillRing emacsKillRing;
+
+    @Test
+    public void testYankPop() throws IllegalAccessException, EmacsKillRingOverflowException {
+            TextEditor text = new TextEditor("test");
+            text.setMark(0);
+            text.setCursor(0);
+            text.yank();
+            Mockito.when(emacsKillRing.currentElt()).thenReturn("test");
+
+    }
+
+    @Test
+    public void testYank() throws IllegalAccessException, EmacsKillRingOverflowException{
+        TextEditor textEditor = new TextEditor("Test");
+
+
+    }
+
+
+
 }
